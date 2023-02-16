1488ba3f7c72740b3092cf4f510c5587748940bb
Hathien99 <inyene007@gmail.com>
2023-02-16 10:37:23 -0800

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - a program that print the side of various data type
+ * Return: 0
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+	
+printf("Size of a char: %lu byte(s)\n", sizeof(a));
+printf("Size of a int: %lu byte(s)\n", sizeof(b));
+printf("Size of a long int: %lu byte(s)\n", sizeof(c));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(d));
+printf("Size of a float: %lu byte(s)\n", sizeof(f));
+return (0);
+}
