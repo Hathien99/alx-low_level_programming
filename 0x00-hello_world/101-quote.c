134ebfc46285165c6ffff88abb4f136b4c5fd931
Hathien99 <inyene007@gmail.com>
2023-02-16 12:01:34 -0800

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,11 @@
-
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - writing a line without printf and put function
+ * Return: 1 (success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
